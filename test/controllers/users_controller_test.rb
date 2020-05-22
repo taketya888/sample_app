c6b270d883c60bb235a8f088e051426f1887ff92
EC2 Default User <ec2-user@ip-172-31-44-152.ap-northeast-1.compute.internal>
2020-05-22 01:37:07 +0000

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,7 +23,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-
+  
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params:{ user: { password:              @other_user.password,
+                                                   password_confirmation: @other_user.password,
+                                                   admin: true } }
+    assert_not @other_user.reload.admin?
+  end
   
   test "should redirect edit when not logged_in as wrong user" do
     log_in_as(@other_user)
