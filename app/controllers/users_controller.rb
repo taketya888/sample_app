8b51249e78682acf9a94f71716a70aa640b71967
EC2 Default User <ec2-user@ip-172-31-44-152.ap-northeast-1.compute.internal>
2020-05-25 07:16:45 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only:  :destroy
+  before_action :admin_user, only:  [:destroy]
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   def show
     @user = User.find(params[:id])
-    
+    redirect_to root_url and return unless @user.activated?
   end
   
   def new
@@ -16,11 +16,11 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    if  @user.save
-      log_in(@user)
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    @user = User.new(user_params)                                               # newビューにて送ったformの中身(nameやemailの値)をuser_paramsで受け取り、ユーザーオブジェクトを生成、@userに代入
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
 
   test "invalid signup information" do
     get signup_path
@@ -16,14 +20,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
   
-  test "valid signup information" do
+  test "valid signup information with account activaton" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
+      post users_path, params: { user: { name:                 "Example User",  # signup_path(/signup)からusers_path(/users)へparamsハッシュのuserハッシュの値を送れるか検証
+                                        email:                 "user@example.com",
+                                        password:              "password",
+                                        password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    @user = assigns(:user)
+    assert_not @user.activated?
+    log_in_as(@user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: @user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(@user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    get edit_account_activation_path(@user.activation_token, email: @user.email)
+    assert @user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
